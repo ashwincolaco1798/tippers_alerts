ab0ad9b2548cfc588b273a71e5f27765f636f36b
ashwincolaco1798 <acolaco@uci.edu>
2022-11-10 16:57:59 -0800

    Added create alert

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from grafana_dashboaring import grafana_utility
 from tippers_logs import logger
 from postgresql_utility import connection
 
-postgres_conn = connection.PostGresUtility()
+postgres_handler = connection.PostGresUtility()
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -19,10 +19,11 @@ CORS(app)
 @app.route('/create_alert', methods = ['POST'])
 def create_alert():
     payload = request.json
-    code, err = verifier.verify_payload(payload,['query','frequency','recurrence','end_time'])
+    code, err = verifier.verify_payload(payload,['query_description','query','frequency','recurrence','end_time'])
     if code != StatusCode.OK:
         logger.tipper_logs(err)
-    
+
+    code, err = postgres_handler.insert_alert(payload['query_description'])
     
     
     
--- postgresql_utility/connection.py
+++ postgresql_utility/connection.py
@@ -2,4 +2,10 @@ import psycopg2
 
 
 class PostGresUtility:
-    conn = psycopg2.connect(host = "caredex-db.ics.uci.edu", database = "tippersdb_uci", user = "postgres", password = "cAredEXpOstgrespW")
+    _conn = psycopg2.connect(host = "caredex-db.ics.uci.edu", database = "tippersdb_uci", user = "postgres", password = "cAredEXpOstgrespW")
+    
+    _cursor = _conn.cursor()
+    
+    def insert_alert(self, query:str):
+        self._cursor.execute("INSERT INTO ALERTS(query, status) VALUES ({},{})".format(query,0)) # 0 for now is False
+        
