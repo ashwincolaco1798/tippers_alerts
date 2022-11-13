71afdb1100be8070c3836daff88ed93cd90eb504
ashwincolaco1798 <acolaco@uci.edu>
2022-11-12 20:07:51 -0800

    added a placeholder till the database comes up

--- grafana_dashboarding/grafana_utility.py
+++ grafana_dashboarding/grafana_utility.py
@@ -7,5 +7,5 @@ class GrafanaHandler:
     _panel = json.load(open("grafana_panel.json","r"))
     _dashboard = json.load(open("grafana_dashboard.json","r"))
     def add_panel(query_desc: str, query: str)-> tuple[StatusCode, str]:
-        
+        return
 
